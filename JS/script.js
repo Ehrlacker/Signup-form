a4e93a45be31ffe878ee47b62629f3f517bee947
Ehrlacker <101287793+Ehrlacker@users.noreply.github.com>
2022-09-21 13:11:28 -0400

    fix styling

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,10 @@
 'use strict'
+
+const firstName = document.querySelector('.first-name');
+const lastName = document.querySelector('.last-name');
+const email = document.querySelector('.email');
+const password = document.querySelector('.password');
+
+
+
+
