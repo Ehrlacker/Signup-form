0200909f2f2ecc705f82d3c5d853fc983e84a9c5
Ehrlacker <101287793+Ehrlacker@users.noreply.github.com>
2022-09-22 09:50:03 -0400

    fix function issue

--- JS/script.js
+++ JS/script.js
@@ -4,7 +4,40 @@ const firstName = document.querySelector('.first-name');
 const lastName = document.querySelector('.last-name');
 const email = document.querySelector('.email');
 const password = document.querySelector('.password');
+const button = document.querySelector('.claim');
+const warning1 = document.querySelector('.one');
+const warning2 = document.querySelector('.two')
+const warning3 = document.querySelector('.three')
+const warning4 = document.querySelector('.four')
 
+
+
+
+//window.addEventListener('DOMContentLoaded', function(){
+warning1.classList.add('hidden');
+warning2.classList.add('hidden');
+warning3.classList.add('hidden');
+warning4.classList.add('hidden');
+
+
+    // warning.forEach(function(e){
+
+    //     e.classList.add('hidden');
+    // })
+//})
+
+
+
+
+button.addEventListener('click', function(){
+
+if (firstName.value.length === 0){
+warning1.classList.remove('hidden'); 
+}else{
+     console.log('yeeesss')
+ }
+
+});
 
 
 
